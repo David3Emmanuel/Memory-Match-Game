f0e01ac1c1002768eb2dca4a6051748c2bd9bb91
David Emmanuel <david3emmanuel@gmail.com>
2024-10-30 16:23:33 +0100

    setup sample cards

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private CardType type;
+
+    private Animator animator;
+    private bool isShowing;
+
+    public bool IsShowing
+    {
+        get => isShowing;
+        set {
+            animator.SetTrigger(value ? "Show" : "Hide");
+            isShowing = value;
+        }
+    }
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnMouseDown()
+    {
+        IsShowing = true;
+        StartCoroutine(FlipBack());
+    }
+
+    IEnumerator FlipBack()
+    {
+        yield return new WaitForSeconds(1.0f);
+        IsShowing = false;
+    }
+}
+
+public enum CardType
+{
+    NONE,
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Blue,
+    Purple,
+}
